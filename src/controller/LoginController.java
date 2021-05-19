a21dc315922c64c8de4a3e783c149a10446f84c0
mattetress <mattetress@me.com>
2021-05-19 10:55:56 -0500

    add zone info

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,6 +1,28 @@
 package controller;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.time.ZoneId;
+import java.time.format.TextStyle;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private Label loginLbl, userIDLbl, passwordLbl, errorLbl, zoneLbl, zoneIDlbl;
+
+    private ZoneId zone = ZoneId.systemDefault();
 
 
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        errorLbl.setText("");
+        zoneLbl.setText(zone.getDisplayName(TextStyle.FULL, Locale.getDefault()));
+    }
 }
