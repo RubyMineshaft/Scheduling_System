e67112359fbcf6a64aae8f56d975d9829c2c9650
mattetress <mattetress@me.com>
2021-05-19 13:37:34 -0500

    add getter for divisions

---
+++ src/DBAccess/DBFirstLevelDivisions.java
@@ -0,0 +1,47 @@
+package DBAccess;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.FirstLevelDivision;
+import util.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+/**
+ * This class performs SQL queries on the first_level_divisions table.
+ */
+public class DBFirstLevelDivisions {
+
+    /** This method queries the database for first level divisions of the specified country.
+     * @param countryID id of the country to get divisions for
+     * @return first level divisions of the country provided
+     */
+    public static ObservableList<FirstLevelDivision> getFirstLevelDivisions(int countryID) {
+        ObservableList<FirstLevelDivision> divisionList = FXCollections.observableArrayList();
+
+        try {
+            String sql = "SELECT * FROM first_level_divisions WHERE COUNTRY_ID = ?";
+
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+            ps.setInt(1, countryID);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int divisionID = rs.getInt("Division_ID");
+                String divisionName = rs.getString("Division");
+
+                FirstLevelDivision division = new FirstLevelDivision(divisionID, divisionName, countryID);
+                divisionList.add(division);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return divisionList;
+    }
+
+}
