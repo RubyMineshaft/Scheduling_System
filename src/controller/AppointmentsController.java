73a9f5ff95952e96bf7070728612618d8c54c102
mattetress <mattetress@me.com>
2021-05-29 11:51:43 -0500

    add appointment view and controller

---
+++ src/controller/AppointmentsController.java
@@ -0,0 +1,70 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AppointmentsController implements Initializable {
+
+    @FXML
+    private Label userLabel;
+
+    @FXML
+    void onAllSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEditAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onManageContacts(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onManageCustomers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMonthSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNewAppointment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onReports(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onWeekSelected(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+}
