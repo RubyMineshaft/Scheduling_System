563dc7c30c24e12d3f146438dba0713373a10ae9
mattetress <mattetress@me.com>
2021-05-30 10:43:31 -0500

    add appointment model

---
+++ src/DBAccess/DBAppointments.java
@@ -0,0 +1,50 @@
+package DBAccess;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Appointment;
+import util.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class DBAppointments {
+
+    public static ObservableList<Appointment> getAllAppointments() {
+        ObservableList<Appointment> appointments = FXCollections.observableArrayList();
+
+        try {
+            String sql = "SELECT a.*, c.Contact_Name FROM appointments a LEFT JOIN contacts c ON a.Contact_ID = c.Contact_ID";
+            PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("Appointment_ID");
+                String title = rs.getString("Title");
+                String description = rs.getString("Description");
+                String location = rs.getString("Location");
+                String type = rs.getString("Type");
+                LocalDateTime start = rs.getTimestamp("Start").toLocalDateTime();
+                LocalDateTime end = rs.getTimestamp("End").toLocalDateTime();
+                LocalDateTime created = rs.getTimestamp("Create_Date").toLocalDateTime();
+                int customerID = rs.getInt("Customer_ID");
+                int userID = rs.getInt("User_ID");
+                int contactID = rs.getInt("Contact_ID");
+                String contactName = rs.getString("Contact_Name");
+
+                Appointment appointment = new Appointment(id, customerID, contactID, title, description, location, type, start, end, created, contactName);
+
+                appointments.add(appointment);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return appointments;
+    }
+
+}
---
+++ src/model/Appointment.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+
+    private int id, customerID, contactID;
+    private String title, description, location, type, contactName;
+    private LocalDateTime start, end, createDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public int getContactID() {
+        return contactID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public Appointment(int id, int customerID, int contactID, String title, String description, String location, String type, LocalDateTime start, LocalDateTime end, LocalDateTime createDate, String contactName) {
+        this.id = id;
+        this.customerID = customerID;
+        this.contactID = contactID;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.type = type;
+        this.start = start;
+        this.end = end;
+        this.createDate = createDate;
+        this.contactName = contactName;
+    }
+}
