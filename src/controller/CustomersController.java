3c7c376f30150294c5122702c3bea6ae1eb0b3e9
mattetress <mattetress@me.com>
2021-05-30 08:56:44 -0500

    add references for tableview data

--- src/controller/CustomersController.java
+++ src/controller/CustomersController.java
@@ -3,22 +3,50 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import model.Customer;
 import model.User;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CustomersController {
+public class CustomersController implements Initializable {
 
     private Stage stage;
     private Parent scene;
 
     @FXML
     private Label userLabel;
+
+    @FXML
+    private TableView<Customer> customersTableView;
+
+    @FXML
+    private TableColumn<Customer, Integer> idCol;
+
+    @FXML
+    private TableColumn<Customer, String> nameCol;
+
+    @FXML
+    private TableColumn<Customer, String> addressCol;
+
+    @FXML
+    private TableColumn<Customer, String> postalCodeCol;
+
+    @FXML
+    private TableColumn<Customer, String> phoneCol;
+
+    @FXML
+    private TableColumn<Customer, String> divisionCol;
+
 
     @FXML
     void onLogOut(ActionEvent event) throws IOException {
@@ -54,5 +82,10 @@ public class CustomersController {
         stage.setScene(new Scene(scene));
         stage.centerOnScreen();
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
